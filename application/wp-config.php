cf653d1e58744497102562be223100c4d827d0d0
bvkathiriya <103038745+bvkathiriya@users.noreply.github.com>
2022-05-24 13:26:04 +0530

    Update wp-config.php

--- application/wp-config.php
+++ application/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', '' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Admin@123' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', '10.96.0.100' );
+define( 'DB_HOST', '' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
